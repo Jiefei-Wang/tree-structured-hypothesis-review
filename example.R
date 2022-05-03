531acd9f7c84fa5f3d66f5d582bbce37885fc0b0
Jiefei-Wang <szwjf08@gmail.com>
2022-05-03 13:50:33 -0400

    update

--- example.R
+++ example.R
@@ -3,6 +3,8 @@ library("BOUTH")
 source("read_data.R")
 source("functions.R")
 
+lengths(apply(taxonomy, 2, function(x) unique(x)))
+
 #FWER
 result_meinshausen <- meinshausen_FWER(taxonomy, pvalues, alpha=0.2, test_fun = stouffer_test)
 lengths(result_meinshausen)
@@ -23,61 +25,3 @@ test.1 <- bouth(anno.table = taxonomy, pvalue.leaves = pvalues,
 
 test.1$results.by.level
 
-
-a=test.1$results.by.node[test.1$results.by.node$is.detected, ]
-a
-a[which(a$otu==" "),]$family
-result_Yekutieli$family[!result_Yekutieli$family%in%a[which(a$otu==" "),]$family]
-
-select <- "Firmicutes_Clostridia_[Mogibacteriaceae]"
-result_Yekutieli$otu[startsWith(result_Yekutieli$otu, select)]
-a$otu[startsWith(a$otu, select)]
-dim(taxonomy[taxonomy$family==select,])
-
-
-
-
-
-library("igraph")
-library("ape")
-library(structSSI)
-nodes <- construct_nodes(taxonomy)
-edges <- c()
-tree.p <- c()
-for(i in rev(seq_along(nodes))){
-  node <- nodes[[i]]
-  tree.p <- c(tree.p, mytest(node$leaf))
-  for(j in node$children){
-    edges <- c(edges,node$name, j)
-  }
-}
-names(tree.p)<-rev(names(nodes))
-
-
-mygraph <- make_graph(edges)
-tree.el <- get.edgelist(mygraph)
-tree.p <- tree.p[V(mygraph)$name]
-hyp.tree <- hFDR.adjust(tree.p, tree.el, 0.2)
-plot(hyp.tree, adjust = FALSE)
-
-summary(hyp.tree)
-
-
-# We can visualize the difference between the unadjusted and the adjusted
-# trees.
-plot(hyp.tree, adjust = FALSE)
-plot(hyp.tree, adjust = TRUE)
-
-
-
-# 
-# data(IBD)
-# dim(IBD$tax.table)
-# colnames(IBD$tax.table)
-# filter <- which(IBD$tax.table$phylum=="Firmicutes")
-# taxonomy <- IBD$tax.table[filter,c("phylum", "species", "otu")]
-# pvalues <- IBD$pvalue.otus[filter]
-# taxonomy1 <- cbind("root", taxonomy)
-# IBD$tax.table[IBD$tax.table$species=="durum",]
-# taxonomy <- IBD$tax.table
-# pvalues <- IBD$pvalue.otus
--- read_data.R
+++ read_data.R
@@ -1,14 +1,23 @@
 library("BOUTH")
 data(IBD)
 
-filter <- which(IBD$tax.table$phylum=="Firmicutes")
-taxonomy <- IBD$tax.table[filter,c("phylum", "class","family", "otu")]
-pvalues <- IBD$pvalue.otus[filter]
+# filter <- which(IBD$tax.table$phylum=="Firmicutes")
+# filter <- 1:nrow(IBD$tax.table)
+# taxonomy <- IBD$tax.table[filter,c("phylum", "class","family", "otu")]
+# taxonomy <- IBD$tax.table[filter,c("phylum", "class","family", "otu")]
+# pvalues <- IBD$pvalue.otus[filter]
+
+
+taxonomy <- IBD$tax.table
+pvalues <- IBD$pvalue.otus
+
+taxonomy <- taxonomy[, colnames(taxonomy)!="species"]
+colnames(taxonomy)[colnames(taxonomy) == "otu"] <- "species"
 
 for(i in 2:(ncol(taxonomy))){
   taxonomy[,i] <- paste0(taxonomy[,i-1],"_",taxonomy[,i])
 }
-names(pvalues) <- taxonomy$otu
+names(pvalues) <- taxonomy$species
 
 
 
